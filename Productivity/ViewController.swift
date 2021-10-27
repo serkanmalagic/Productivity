b10c667a1b120f6df51822f09be7a8113cd8930e
Serkan Mehmet MALAGİÇ <32618577+serkanmalagic@users.noreply.github.com>
2021-10-28 02:08:06 +0300

    functions added

--- Productivity/ViewController.swift
+++ Productivity/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
+struct User {
+    var firstName: String
+}
+
+var users = [
+    User(firstName: "Serkan"),
+    User(firstName: "Gül"),
+    User(firstName: "Şansal"),
+    User(firstName: "Erman"),
+    User(firstName: "Amil")
+]
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        checkVariableType("Serkan", myType: String.self)
+        getAnyTypes(12, 12)
+        getAnyTypes("12", "12")
+        getAnyTypes(12.1, 12.1)
+        sortDatasArray()
+        sortStructs()
+
     }
 
+    //  Bu fonksiyon gönderilen parametreyle değişken tipini kontrol etmektedir.
+    func checkVariableType<T>(_ a: Any, myType: T.Type) {
+        if let a = a as? T {
+            print("\(a) ve \(myType) eşleşti")
+        }
+    }
+    
+    func getAnyTypes(_ x : Any , _ y : Any ){
+        print("Değerler : \(x), \(y)")
+    }
+    
+    func sortDatasArray(){
+        var strArr = ["Serkan", "Gül", "Şansal", "Erman", "Amil"]
+        strArr.sort()
+        
+        print(strArr)
+    }
+    
+    
+    func sortStructs(){
+        users.sort {
+            $0.firstName < $1.firstName
+        }
+        print(users)
+    }
 
 }
 
